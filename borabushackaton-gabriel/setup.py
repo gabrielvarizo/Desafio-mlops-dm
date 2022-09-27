fe94e956bd73b79e23f35e04d0d3419e25bee060
gabrielvarizo <98413062+gabrielvarizo@users.noreply.github.com>
2022-09-27 10:46:35 -0300

    Alterações com dados de teste

---
+++ borabushackaton-gabriel/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Desafio que visa o aprendizado do uso de pipelines e do MLFlowow',
+    author='Gabriel Varizo',
+    license='MIT',
+)
